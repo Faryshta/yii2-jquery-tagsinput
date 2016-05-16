6babfe5d3cb5b02b4155ceade8888b9b50562f43
Angel Guevara <aguevara@tecnocen.com>
2016-05-16 17:09:12 -0500

    closes #1 adding testing to autocomplete_url

--- tests/JqueryTagsTest.php
+++ tests/JqueryTagsTest.php
@@ -2,8 +2,10 @@
 namespace faryshta\tests;
 
 use faryshta\widgets\JqueryTagsInput;
+use yii\base\InvalidConfigException;
+
 /**
- * Test the functionality for the enum extension
+ * Test the functionality for the jQuery tags input plugin
  */
 class JqueryTagsTest extends TestCase
 {
@@ -13,9 +15,23 @@ class JqueryTagsTest extends TestCase
 <input type="text" id="w0" class="form-control" name="tags" value="yii2,jquery">
 HTML;
 
-		$this->assertEquals($expected, JqueryTagsInput::widget([
+        $this->assertEquals($expected, JqueryTagsInput::widget([
             'name' => 'tags',
             'value' => 'yii2,jquery',
         ]));
+    }
+
+    public function testAutocompleteException()
+    {
+        try {
+            JqueryTagsInput::widget([
+                'name' => 'tags',
+                'value' => 'yii2,jquery',
+                'clientOptions' => ['autocomplete_url' => ''],
+            ]);
+            $this->fail('Expected exception was not thrown');
+        } catch (InvalidConfigException $e) {
+            $this->assertNotEmpty($e->getMessage());
+        }
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,14 +8,3 @@ $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 Yii::setAlias('@faryshta', dirname(__DIR__));
-
-if (!class_exists('yiiunit\TestCase')) {
-    if (is_file(__DIR__ . '/../../../tests/TestCase.php')) {
-        require_once(__DIR__ . '/../../../tests/TestCase.php');
-    } elseif (is_file(__DIR__ . '/BaseTestCase.php')) {
-        require_once(__DIR__ . '/BaseTestCase.php');
-    } else {
-        file_put_contents(__DIR__ . '/BaseTestCase.php', file_get_contents('https://raw.githubusercontent.com/yiisoft/yii2/master/tests/TestCase.php'));
-        require_once(__DIR__ . '/BaseTestCase.php');
-    }
-}
